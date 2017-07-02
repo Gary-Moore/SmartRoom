8a949f329e5084dff8c90494c0af9b3c1e24858d
Gary Moore <garyprm@outlook.com>
2017-07-02 23:06:42 +0100

    Initial checkin

--- SmartRoom.RoomMonitorApp/StartupTask.cs
+++ SmartRoom.RoomMonitorApp/StartupTask.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Net.Http;
+using System.Threading;
 using Windows.ApplicationModel.Background;
+using Windows.Devices.Gpio;
+using Windows.UI.Xaml;
+using Sensors.Dht;
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
@@ -11,6 +15,12 @@ namespace SmartRoom.RoomMonitorApp
 {
     public sealed class StartupTask : IBackgroundTask
     {
+        private GpioPin _temperaturePin;
+
+        private Dht11 _dht11;
+        private Timer _timer;
+
+
         public void Run(IBackgroundTaskInstance taskInstance)
         {
             // 
@@ -20,6 +30,36 @@ namespace SmartRoom.RoomMonitorApp
             // from closing prematurely by using BackgroundTaskDeferral as
             // described in http://aka.ms/backgroundtaskdeferral
             //
+
+            InitHardware();
+        }
+
+        private void InitHardware()
+        {
+            _temperaturePin = GpioController.GetDefault().OpenPin(4, GpioSharingMode.Exclusive);
+
+            _dht11 = new Dht11(_temperaturePin, GpioPinDriveMode.Input);
+
+            _timer = new Timer(Timer_Tick, null, 0, 10000);
+        }
+
+        private async void Timer_Tick(object sender)
+        {
+            try
+            {
+                DhtReading reading = new DhtReading();
+                reading = await _dht11.GetReadingAsync(3).AsTask();
+
+                if (reading.IsValid)
+                {
+                    Console.WriteLine(reading.Temperature);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
